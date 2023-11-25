6bcbc19fb76c51727ab813ec6aab5abeea75e91a
Fran3003 <franco.rantucho@gmail.com>
2023-11-24 22:31:37 -0300

    Component Header finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Header } from '@/components/Header'
 import Navbar from '@/components/NavBar/Navbar'
 import type { Metadata } from 'next'
 
@@ -10,6 +11,7 @@ export default function Home() {
   return (
     <main className="bg-[#363062] h-[100vh]">
       <Navbar />
+      <Header />
     </main>
   )
 }
---
+++ src/components/Header/dataHeader.tsx
@@ -0,0 +1,27 @@
+import { FaWhatsapp, FaTelegram  } from "react-icons/fa";
+import { FaFileAlt, FaLinkedin, FaGithub  } from "react-icons/fa";
+
+
+export const socialNetworks = [
+  {
+    logo: <FaGithub size="40" />,
+    src: "https://github.com/Fran3003",
+  },
+  {
+    logo: <FaFileAlt size="40" />,
+    src: "/CV-Franco Nuevo.pdf",
+  },
+  {
+    logo: <FaLinkedin size="40" />,
+    src: "https://www.linkedin.com/in/franco-emanuel-rantucho/",
+  },
+  {
+    logo: <FaWhatsapp size="40" />,
+    src: "https://wa.me/2923439562",
+  },
+  {
+    logo: <FaTelegram size="40" />,
+    src: "https://t.me/Franco94",
+  },
+  
+];
